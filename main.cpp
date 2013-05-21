34f88264f91eca8d387fee5e174beb8ca4fffa05
Denis Demidov <ddemidov@ksu.ru>
2013-05-21 22:55:54 +0400

    Ditch uBlas matrix, replace it with vector of maps.
    Also switch to Eigen interface for amgcl.

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@
 #include <viennafem/fem.hpp>
 #include <viennafem/io/vtk_writer.hpp>
 #include <viennamath/expression.hpp>
+#include <viennacl/tools/adapter.hpp>
 
+#include <Eigen/Dense>
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
 #include <boost/numeric/ublas/vector.hpp>
-#include <boost/numeric/ublas/matrix_sparse.hpp>
 
 #include <amgcl/amgcl.hpp>
 #include <amgcl/interp_smoothed_aggr.hpp>
 #include <amgcl/aggr_plain.hpp>
 #include <amgcl/level_cpu.hpp>
-#include <amgcl/operations_ublas.hpp>
+#include <amgcl/operations_eigen.hpp>
 #include <amgcl/bicgstab.hpp>
 #include <amgcl/profiler.hpp>
 
 #include "contour.h"
 #include "mesher.h"
 
-typedef boost::numeric::ublas::compressed_matrix<double> ublas_matrix;
 typedef boost::numeric::ublas::vector<double> ublas_vector;
 
 // Solve equation laplace(u) = 1 on a semicircle.
@@ -47,31 +49,66 @@ int main(int argc, char *argv[]) {
             viennafem::set_dirichlet_boundary(*v, 0.0);
     }
 
-    ublas_matrix A;
-    ublas_vector f;
 
+    amgcl::sparse::matrix<double, int> A;
     viennafem::pde_assembler fem_assembler;
+    Eigen::VectorXd f;
 
-    fem_assembler(viennafem::make_linear_pde_system(poisson, u), domain, A, f);
+    {
+        std::vector< std::map<unsigned, double> > A_tmp;
+        viennacl::tools::sparse_matrix_adapter<double> A_proxy(A_tmp);
+        ublas_vector f_tmp;
+
+        fem_assembler(viennafem::make_linear_pde_system(poisson, u), domain,
+                A_proxy, f_tmp);
+
+        // Copy matrix to CSR format, wrap it into Eigen type.
+        size_t n   = A_tmp.size();
+        size_t nnz = std::accumulate(A_tmp.begin(), A_tmp.end(), 0UL,
+                [](size_t sum, const std::map<unsigned, double> &r) {
+                    return sum + r.size();
+                });
+
+        A.rows = A.cols = n;
+        A.row.reserve(A_tmp.size() + 1);
+        A.col.reserve(nnz);
+        A.val.reserve(nnz);
+
+        A.row.push_back(0);
+        for(auto r = A_tmp.begin(); r != A_tmp.end(); ++r) {
+            for(auto v = r->begin(); v != r->end(); ++v) {
+                A.col.push_back(std::get<0>(*v));
+                A.val.push_back(std::get<1>(*v));
+            }
+            A.row.push_back(A.col.size());
+        }
+
+        f.resize(n);
+        std::copy(f_tmp.begin(), f_tmp.end(), &f[0]);
+    }
+
     prof.toc("assemble");
-
-    ublas_vector x(f.size(), 0.0);
 
     // Solve the assembled system with amgcl
     typedef amgcl::solver<
-        double, ptrdiff_t,
+        double, int,
         amgcl::interp::smoothed_aggregation<amgcl::aggr::plain>,
         amgcl::level::cpu<amgcl::relax::spai0>
         > AMG;
 
     prof.tic("solve");
     prof.tic("setup");
-    AMG amg( amgcl::sparse::map(A), AMG::params() );
+    AMG amg( A, AMG::params() );
     prof.toc("setup");
 
     std::cout << amg << std::endl;
 
-    auto cnv = amgcl::solve(A, f, amg, x, amgcl::bicg_tag());
+    Eigen::MappedSparseMatrix<double, Eigen::RowMajor, int> A_eigen(
+            A.rows, A.cols, A.row.back(), A.row.data(), A.col.data(), A.val.data()
+            );
+    Eigen::VectorXd x = Eigen::VectorXd::Zero(A.rows), rhs(A.rows);
+
+    amgcl::solve(A_eigen, f, amg, x, amgcl::bicg_tag());
     prof.toc("solve");
 
     // Save the result.
